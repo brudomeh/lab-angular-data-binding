421015a280f81c2dcbc5ae2c7e02245e4ba78ec3
Bruno <brunodomecq@gmail.com>
2018-07-24 17:25:10 +0200

    done menos el bonus

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import foods from '../foods';
+import { Component, OnInit } from "@angular/core";
+import foods from "../foods";
 
 @Component({
-  selector: 'app-food-list',
-  templateUrl: './food-list.component.html',
-  styleUrls: ['./food-list.component.css']
+  selector: "app-food-list",
+  templateUrl: "./food-list.component.html",
+  styleUrls: ["./food-list.component.css"]
 })
 export class FoodListComponent implements OnInit {
+  product: Array<object>;
+  newFood: Object = {};
+  food: Object = {}
+  patern:string
+  constructor() {}
+  apear = {
+    desaparecer: false
+  }
+  list: Array<object>=[]
 
-  constructor() { }
 
   ngOnInit() {
+    this.product = foods
   }
 
+  addFood() {
+    console.log("Add food has been called");
+    this.product.push(this.newFood);
+    this.newFood = {};
+    this.apear.desaparecer = true
+  }
+
+
+  add (food){
+    this.list.push(food);
+    console.log(this.list)
+  }
 }
